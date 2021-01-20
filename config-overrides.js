db4323d562e3871639cbbf5e635c357484e7dd2e
ChuSong327 <chuchusong23@gmail.com>
2021-01-20 13:58:50 -0800

    initial commit.

---
+++ config-overrides.js
@@ -0,0 +1,11 @@
+// config-overrides.js
+module.exports = function override(config, env) {
+    //return config;
+    config.optimization.runtimeChunk = false;
+    config.optimization.splitChunks = {
+        cacheGroups: {
+            default: false,
+        },
+    };
+    return config;
+}
---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from "react";
+
+function App() {
+  return (
+    <div>
+      <header>
+        <h3>This is Excelsheet</h3>
+      </header>
+    </div>
+  );
+}
+
+export default App;
